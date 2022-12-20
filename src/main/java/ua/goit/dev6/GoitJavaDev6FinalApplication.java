b72a54f3f623fcae9ed8e2d5164fc642b75f7769
serhiibabanov <serhii.babanov@gmail.com>
2022-12-19 20:11:43 -0500

    init commit

---
+++ src/main/java/ua/goit/dev6/GoitJavaDev6FinalApplication.java
@@ -0,0 +1,13 @@
+package ua.goit.dev6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoitJavaDev6FinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoitJavaDev6FinalApplication.class, args);
+    }
+
+}
