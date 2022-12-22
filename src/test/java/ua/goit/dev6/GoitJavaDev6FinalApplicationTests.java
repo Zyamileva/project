9ef0f080a807fd3195695349e62ec2fb6ca9ec8a
serhiibabanov <serhii.babanov@gmail.com>
2022-12-21 22:08:55 -0500

    disable validation

--- src/test/java/ua/goit/dev6/GoitJavaDev6FinalApplicationTests.java
+++ src/test/java/ua/goit/dev6/GoitJavaDev6FinalApplicationTests.java
@@ -2,8 +2,10 @@ package ua.goit.dev6;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GoitJavaDev6FinalApplicationTests {
 
     @Test
